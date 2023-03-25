d0de255248b38187f46506149b83a94a9bac3afb
Suhwa Han <suhwa@utexas.edu>
2023-03-25 14:26:59 -0500

    Combine different evaluation functions into one generalized evaluation function

--- Evaluation.R
+++ Evaluation.R
@@ -1,12 +1,19 @@
 ### Functions to conduct evaluations ----
 
 ###   Calculate power/type I error for SGLRT -------------------
-calcPw_SGLRT <- function (Proc,Res, CV, e_dt_pt_excl, sampling, c.pt, st_eval, idx_ab_p){
+calcPw_SGLRT <- function (Proc, Res, CV, e_dt_pt=NULL, sampling, c.pt, n_rf_s, idx_ab_p){
   
   neval <- length(Res[1,])
   Nexaminee <- length(Res[,1])
-  dt_cht <- abs(Res) > abs(CV) # View(dt_cht) # LR has been set to be higher when the statistic favors H1
-  dt_pt <- matrix(NA, Nexaminee, 2, dimnames=list(NULL, c("idx", "dt.pt_eval")))  # matrix containing idx of people who were "detected" anywhere along the items (power+type1) and their detection point; 1st column=index; 2nd column=detection point  # View(dt_pt)
+  
+  # Find a cell which exceeded CV ------------
+  dt_cht <- abs(Res) > abs(CV)  # LR has been set to be higher when the statistic favors H1
+  
+  # matrix containing idx of people who were "detected" anywhere along the items (power+type1) and their detection point; 
+  # 1st column=index; 2nd column=detection point  
+  
+  dt_pt <- matrix(NA, Nexaminee, 2, dimnames=list(NULL, c("idx", "dt.pt_eval")))  
+  
   k <- 0  # counting variable for examinee
   for (i in 1:Nexaminee){ 
     found <- FALSE
@@ -21,15 +28,28 @@ calcPw_SGLRT <- function (Proc,Res, CV, e_dt_pt_excl, sampling, c.pt, st_eval, i
       j <- j+1
     }
   }
+  
   tmp <- which(is.na(dt_pt[,1]))  # row number for NAs in `dt_pt`
   dt_pt <- dt_pt[-tmp,]  # entire detected ppl
   pw_tent_mat <- dt_pt[which(dt_pt[,"idx"] %in% idx_ab_p),, drop = F] # Correctly detected ppl (including early detection)
   f_dt_mat <- dt_pt[-which(dt_pt[,"idx"] %in% idx_ab_p),, drop = F] # Incorrectly detected ppl (false detection t1 error)  # earliest detection point in the detection chart
-  if (sampling == "exclusive"){
-    e_dt_pt <- e_dt_pt_excl
-  } else {
-    e_dt_pt <- c.pt-st_eval+1
+  
+  # Define premature detection points for each procedure
+  if (Proc == "CUSUM_obs"){ # CUSUM control chart with manifest indicators
+    st_eval = n_rf_s+1
+    e_dt_pt = c.pt-st_eval+1
+  } else if (Proc == "CUSUM_est" && !is.null(e_dt_pt)){
+    st_eval = n_rf_s*2
+    e_dt_pt = e_dt_pt
+  } else if (Proc == "SGLRT" && sampling = "exclusive" && !is.null(e_dt_pt)){
+    st_eval = n_rf_s*2
+    e_dt_pt = e_dt_pt
+  } else if (Proc == "SGLRT" && sampling = "ref_mov_fixed"){
+    st_eval = n_rf_+1
+    e_dt_pt = c.pt-st_eval+1
   }
+  
+  # Find prematurely detected cases --------
   if (c.pt >= st_eval){
     e_dt_mat <- pw_tent_mat[which(pw_tent_mat[,"dt.pt_eval"] < e_dt_pt),, drop = F]  # index and dt point of early detected t1 error
     pw_mat <- pw_tent_mat[which(pw_tent_mat[,"dt.pt_eval"] >= e_dt_pt),, drop = F]  # index and dt point of true power ppl
@@ -38,6 +58,8 @@ calcPw_SGLRT <- function (Proc,Res, CV, e_dt_pt_excl, sampling, c.pt, st_eval, i
     pw_mat <- pw_tent_mat
   }
   t1_mat <- rbind(f_dt_mat, e_dt_mat)
+  
+  # Power, Type I, early detection rates ---------
   e_dt_rat <- length(e_dt_mat[,"idx"])/length(pw_tent_mat[,"idx"])
   pw_rat <- length(pw_mat[,"idx"])/length(idx_ab_p)
   t1_rat <- length(t1_mat[,"idx"])/(Nexaminee-length(idx_ab_p))
@@ -48,6 +70,7 @@ calcPw_SGLRT <- function (Proc,Res, CV, e_dt_pt_excl, sampling, c.pt, st_eval, i
     ARL1 <- mean(pw_mat[,"dt.pt_eval"]+st_eval-c.pt) # has to add (st_eval-c.pt); assuming no detection happened before the eval point
     ARL0 <- mean(f_dt_mat[,"dt.pt_eval"]) # ARL0 is calculated from the onset of evaluation point
   }
+  
   return (list(
     pw_rat=pw_rat, t1_rat=t1_rat, ARL1=ARL1,
     ARL0=ARL0, e_dt_rat=e_dt_rat, CV=CV, neval=neval
